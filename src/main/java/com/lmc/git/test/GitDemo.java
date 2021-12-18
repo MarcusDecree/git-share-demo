b891fa5dd10ec7ccc33c216db71a09a7dc722871
lmc <1074618382@qq.com>
2021-12-18 18:40:50 +0800

    idea first commit

---
+++ src/main/java/com/lmc/git/test/GitDemo.java
@@ -0,0 +1,12 @@
+package com.lmc.git.test;
+
+/**
+ * author  XiaoBai
+ * date    2021/12/18 18:22
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("git demo");
+    }
+
+}
